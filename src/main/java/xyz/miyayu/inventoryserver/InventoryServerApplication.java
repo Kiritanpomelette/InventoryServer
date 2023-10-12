0e517989df090e0f9f10fd974830d59143017289
Miyayu <yuchan2215@gmail.com>
2023-10-12 13:38:40 +0900

    First Commit

---
+++ src/main/java/xyz/miyayu/inventoryserver/InventoryServerApplication.java
@@ -0,0 +1,13 @@
+package xyz.miyayu.inventoryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/xyz/miyayu/inventoryserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package xyz.miyayu.inventoryserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(InventoryServerApplication.class);
+    }
+
+}
